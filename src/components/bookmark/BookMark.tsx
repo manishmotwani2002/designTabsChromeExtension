06541df46a6d02b3e4bc795557ff8b8dd6b1eb6b
manishmotwani2002 <manishmotwani2002@gmail.com>
2022-07-21 18:57:24 +0530

    additional fixes

--- src/components/bookmark/BookMark.tsx
+++ src/components/bookmark/BookMark.tsx
@@ -36,18 +36,49 @@ const BookMark = ({ bookmarks, removeBookmark, updateBookmark }) => {
           <img
             src={`https://www.google.com/s2/favicons?sz=64&domain_url=${bookmark.text}`}
             alt="Bookmark"
-            style={{ width: "40px", height: "40px" }}
+            style={{ width: "30px", height: "30px", padding: "5px" }}
           />
         </a>
       </div>
+      <div style={{ whitespace: "nowrap" }}>{bookmark.text}</div>
       <div>
-        <button onClick={() => removeBookmark(bookmark.id)}>Remove</button>
+        <span onClick={() => removeBookmark(bookmark.id)}>
+          <svg
+            style={{ height: "30px", width: "30px", padding: "5px" }}
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            stroke-width="2"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
+            />
+          </svg>
+        </span>
 
-        <button
+        <span
           onClick={() => setEdit({ id: bookmark.id, value: bookmark.text })}
         >
-          Edit
-        </button>
+          <svg
+            style={{ height: "30px", width: "30px", padding: "5px" }}
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            stroke-width="2"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"
+            />
+          </svg>
+        </span>
       </div>
     </div>
   ));
--- src/components/todo/Todo.tsx
+++ src/components/todo/Todo.tsx
@@ -32,17 +32,43 @@ const Todo = ({ todos, removeTodo, updateTodo }) => {
   return todoList.map((todo, index) => (
     <div key={index} className="todo_item">
       <div key={todo.id}>{todo.text}</div>
-      <div>
-        <button className="btn" onClick={() => removeTodo(todo.id)}>
-          Remove
-        </button>
 
-        <button
-          className="btn"
-          onClick={() => setEdit({ id: todo.id, value: todo.text })}
-        >
-          Edit
-        </button>
+      <div>
+        <span onClick={() => removeTodo(todo.id)}>
+          <svg
+            style={{ height: "30px", width: "30px", padding: "5px" }}
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            stroke-width="2"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
+            />
+          </svg>
+        </span>
+
+        <span onClick={() => setEdit({ id: todo.id, value: todo.text })}>
+          <svg
+            style={{ height: "30px", width: "30px", padding: "5px" }}
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            stroke-width="2"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"
+            />
+          </svg>
+        </span>
       </div>
     </div>
   ));
